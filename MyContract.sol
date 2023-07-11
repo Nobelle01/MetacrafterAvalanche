7c7ca9923ce4712ce57d6bc258bbf1a4d944b81e
Nobelle01 <126451229+Nobelle01@users.noreply.github.com>
2023-07-11 09:23:08 +0800

    Update and rename SmartContract.sol to MyContract.sol

---
+++ MyContract.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 public value;
+
+    function setValue(uint256 _value) external {
+        // Using require() to validate a condition
+        require(_value > 0, "Value must be greater than zero");
+
+        // Using assert() to assert an invariant condition
+        assert(value != _value);
+
+        // Performing some operations
+
+        // Using revert() to revert the transaction with a custom message
+        if (_value == 42) {
+            revert("Cannot set the value to 42");
+        }
+
+        // Setting the value
+        value = _value;
+    }
+}
